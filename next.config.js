baecfc7483cef2f4db45cf9f127960bc6e5bf02b
Rudraksh Saha <Rud@ruds-device.local>
2025-08-30 23:38:37 +1000

    Remove swcMinify warning by using default SWC minification

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  swcMinify: false,
   images: {
     domains: ['firebasestorage.googleapis.com', 'localhost'],
     formats: ['image/webp', 'image/avif'],
